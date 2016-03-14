09985a0603949807dbc1508637510f404eccc04f
ceekay1991 <374993061@qq.com>
2016-03-14 15:24:39 +0800

    Initial Commit

---
+++ UIMenuController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UIMenuController
+//
+//  Created by chenronghang on 16/3/14.
+//  Copyright © 2016年 com.crh.objc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
